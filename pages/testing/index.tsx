720362d298567786ed72a152f61adcc41872670c
JustDankas <jan-haspel@hotmail.com>
2023-01-27 22:53:33 +0200

    dadas

---
+++ pages/testing/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Component } from "react";
+import styles from "@/styles/Inner.module.css";
+function Testing() {
+  return <div className={styles.main}>This is testing page</div>;
+}
+
+export default Testing;
